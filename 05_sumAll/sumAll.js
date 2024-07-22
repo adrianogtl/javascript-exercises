a3f689d1f8a38a70e82cc12863786a9d740a4a23
adrianogtl <80999356+adrianogtl@users.noreply.github.com>
2024-07-22 11:01:02 -0300

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (arg1, arg2) {
+  const argNegative = arg1 < 0 || arg2 < 0;
+  const nonInteger = !Number.isInteger(arg1) || !Number.isInteger(arg2);
+  if (argNegative || nonInteger) {
+    return "ERROR";
+  }
 
+  let start = 0;
+  let end = 0;
+
+  if (arg1 < arg2) {
+    start = arg1;
+    end = arg2;
+  } else {
+    start = arg2;
+    end = arg1;
+  }
+
+  const interval = [];
+  for (let i = start; i <= end; i++) {
+    interval.push(i);
+  }
+  return interval.reduce((a, b) => a + b);
 };
 
 // Do not edit below this line
