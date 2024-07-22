35d2ed49d5d4250ad8358898f03ff5197d52e9f6
adrianogtl <80999356+adrianogtl@users.noreply.github.com>
2024-07-22 20:02:47 -0300

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemp) {
+  const celsiusTemp = (fahrenheitTemp - 32) * (5 / 9);
+  if (!Number.isInteger(celsiusTemp)) {
+    return Number(celsiusTemp.toFixed(1));
+  }
+  return celsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusTemp) {
+  const fahrenheitTemp = celsiusTemp * (9 / 5) + 32;
+  if (!Number.isInteger(fahrenheitTemp)) {
+    return Number(fahrenheitTemp.toFixed(1));
+  }
+  return fahrenheitTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
