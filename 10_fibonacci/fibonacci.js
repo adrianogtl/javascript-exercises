6322a1ce2edde4ded6e6160696d5d52af6420434
adrianogtl <80999356+adrianogtl@users.noreply.github.com>
2024-08-13 19:08:26 -0300

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let count = n;
+  if (typeof n === "string") count = parseInt(n);
+  if (count < 0) return "OOPS";
+  if (count === 1) return 1;
 
+  let pastNum = 0;
+  let lastNum = 1;
+  let current = 0;
+
+  for (let i = 2; i <= count; i++) {
+    current = pastNum + lastNum;
+    pastNum = lastNum;
+    lastNum = current;
+  }
+
+  return current;
 };
 
 // Do not edit below this line
