3f75c23abfb445d89919f5952391d2eb7facf12f
adrianogtl <80999356+adrianogtl@users.noreply.github.com>
2024-08-13 10:09:19 -0300

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const clearedString = string.replace(/[^A-z0-9]/g, "").toLowerCase();
+  const reversedString = clearedString.split("").reverse().join("");
+  return clearedString === reversedString ? true : false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
