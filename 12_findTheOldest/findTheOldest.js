f47e3f68cb870b7ac16c1361e8cae42e3dfbbb32
adrianogtl <80999356+adrianogtl@users.noreply.github.com>
2024-08-14 16:17:13 -0300

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const getAge = (birth, death) => {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(
+      oldestPerson.yearOfBirth,
+      oldestPerson.yearOfDeath
+    );
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldestPerson;
+  });
 };
 
 // Do not edit below this line
